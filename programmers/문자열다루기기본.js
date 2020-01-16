ef7a228c1da1719fa45a65f4f1f105a5abe6f2a5
zooo1 <marulover7@gmail.com>
2020-01-16 14:35:22 +0900

    [ADD] 문자열다루기기본 2번째 풀이

--- programmers/문자열다루기기본.js
+++ programmers/문자열다루기기본.js
@@ -1,9 +1,4 @@
 function solution(s) {
-    if (s.length == 4 || s.length == 6) {
-        for(var i=0; i<s.length ; i++){
-            if (isNaN(s[i])) return false;
-        }
-        return true;
-    }
-    else return false;
+    if ((s.length === 4 || s.length === 6) && (s == parseInt(s))) return true;    
+    return false;
 }
